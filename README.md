0f12a98b495f4ad14664427c2ae6967365430279
Graham Neubig <neubig@gmail.com>
2019-11-02 07:24:42 -0400

    A bunch of changes, including adding reviewer matching

--- README.md
+++ README.md
@@ -1,17 +1,46 @@
-# simple-and-effective-paraphrastic-similarity
+# ACL Reviewer Matching Code
 
-Code to train models from "Simple and Effective Paraphrastic Similarity from Parallel Translations".
+This is an initial pass at code to match reviewers for the [ACL Conferences](http://aclweb.org).
+It is based on paper matching between abstracts of submitted papers and a database of papers from
+[Semantic Scholar](https://www.semanticscholar.org).
 
-The code is written in Python 3.7 and requires numpy, scipy, sentencepiece, sacremoses, and PyTorch >= 1.0 libraries.
+## Installing
 
-To get started, download the data files used for training from http://www.cs.cmu.edu/~jwieting and download the STS evaluation data:
+**Step 1:** install requirements:
 
-    wget http://www.cs.cmu.edu/~jwieting/acl19-simple.zip
-    unzip acl19-simple.zip
-    rm acl19-simple.zip
-    bash download_sts17.sh
+    pip install -r requirements.txt
+    
+**Step 2:** Download the semantic scholar dump
+[following the instructions](http://api.semanticscholar.org/corpus/download/).
 
-If you use our code, models, or data for your work please cite:
+**Step 3:** Wherever you downloaded the semantic scholar dump, make a virtual link to the `s2` directory here.
+
+    ln -s /path/to/semantic/scholar/dump s2
+
+**Step 4:** Make a scratch directory where we'll put working files
+
+    mkdir -p scratch/
+    
+**Step 5:** For efficiency purposes, we'll filter down the semantic scholar dump to only papers where it has a link to
+aclweb.org, a rough approximation of the papers in the ACL Anthology.
+
+    zcat s2/s2-corpus*.gz | grep aclweb.org > scratch/acl-anthology.json
+
+**Step 6:** In the scratch directory, create two files of paper abstracts `scratch/abstracts.txt` and reviewer names
+`scratch/reviewers.txt`.
+
+**Step 7:** Train a model of semantic similarity based on the ACL anthology.
+
+TODO: finish up directions
+
+## Method Description
+
+This method works by calculating the similarity between papers, then finding reviewers that have several (by default
+up to 3) papers similar to the submitted one. Lower-ranked papers are slightly downweighted, allow for reviewers with
+even one highly relevant paper to get more highly matched.
+
+The method for measuring similarity of paper abstracts is based on the token embedding averaging method in the following
+paper:
 
     @inproceedings{wieting19simple,
         title={Simple and Effective Paraphrastic Similarity from Parallel Translations},
@@ -21,14 +50,7 @@ If you use our code, models, or data for your work please cite:
         year={2019}
     }
 
-To train sp-average models in language xx on GPU, choices are es, ar, or tr:
+# Credits
 
-    python main.py --data-file acl19-simple/en-xx.os.1m.tok.20k.txt --model avg --dim 300 --epochs 10 --share-vocab 1 --dropout 0.3 --sp-model acl19-simple/en-xx.os.1m.tok.sp.20k.model
+This code was written/designed by Graham Neubig, John Wieting, Arya McCarthy, and Amanda Stent.
 
-To train trigram-average models in language xx on GPU:
-
-    python main.py --data-file acl19-simple/en-xx.os.1m.tok.txt --model avg --dim 300 --epochs 10 --ngrams 3 --share-vocab 1 --dropout 0.3
-
-To train the bidirectional LSTM models in language xx on GPU:
-
-    python main.py --data-file acl19-simple/en-xx.os.1m.tok.20k.txt --model lstm --dim 300 --epochs 10 --dropout 0.3 --scramble-rate 0.3 --share-vocab 1 --share-encoder 1 --sp-model acl19-simple/en-xx.os.1m.tok.sp.20k.model
--- suggest_reviewers.py
+++ suggest_reviewers.py
@@ -2,64 +2,26 @@
 command: OMP_NUM_THREADS=12 python  nn.py --model avg --dim 300 --epochs 10 --ngrams 3 --share-vocab 1 --dropout 0.3 --gpu 0 --save-every-epoch 1 --nn-file s2/s2-filtered-aclweb.abs --load-file model_1.pt 
 """
 
-import io
-import sentencepiece as spm
-from scipy.stats import spearmanr
-from scipy.stats import pearsonr
 import json
 from utils import Example, unk_string
 from sacremoses import MosesTokenizer
 import argparse
-from models import Averaging, LSTM, load_model
+from models import load_model
+from collections import defaultdict
 import numpy as np
 import sys
 sys.setrecursionlimit(10000)
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument("--submission_file", type=str, required=True, help="A line-by-line file of submissions")
-parser.add_argument("--db_file", type=str, required=True, help="File (in s2 json format) of relevant papers from reviewers")
-parser.add_argument("--reviewer_file", type=str, required=True, help="A file of reviewer files or IDs that can review this time")
-parser.add_argument("--filter_field", type=str, default="Name", help="Which field to filter on")
-parser.add_argument("--model_file", help="filename to load the pre-trained semantic similarity file.")
-parser.add_argument("--save_matrix", help="A filename for where to save the similarity matrix")
-parser.add_argument("--load_matrix", help="A filename for where to load the cached similarity matrix")
-parser.add_argument("--ngrams", default=0, type=int, help="whether to use character n-grams")
-
-# parser.add_argument("--gpu", default=1, type=int, help="whether to train on gpu")
-# parser.add_argument("--dim", default=300, type=int, help="dimension of input embeddings")
-# parser.add_argument("--model", default="avg", choices=["avg", "lstm"], help="type of base model to train.")
-# parser.add_argument("--grad-clip", default=5., type=float, help='clip threshold of gradients')
-# parser.add_argument("--epochs", default=10, type=int, help="number of epochs to train")
-# parser.add_argument("--lr", default=0.001, type=float, help="learning rate")
-# parser.add_argument("--dropout", default=0., type=float, help="dropout rate")
-# parser.add_argument("--batchsize", default=128, type=int, help="size of batches")
-# parser.add_argument("--megabatch-size", default=60, type=int, help="number of batches in megabatch")
-# parser.add_argument("--megabatch-anneal", default=150., type=int, help="rate of megabatch annealing in terms of "
-#                                                                        "number of batches to process before incrementing")
-# parser.add_argument("--pool", default="mean", choices=["mean", "max"], help="type of pooling")
-# parser.add_argument("--zero-unk", default=1, type=int, help="whether to ignore unknown tokens")
-
-# parser.add_argument("--save-every-epoch", default=0, type=int, help="whether to save a checkpoint every epoch")
-# parser.add_argument("--outfile", default="model", help="output file name")
-# parser.add_argument("--hidden-dim", default=150, type=int, help="hidden dim size of LSTM")
-# parser.add_argument("--delta", default=0.4, type=float, help="margin")
-# parser.add_argument("--share-encoder", default=1, type=int, help="whether to share the encoder (LSTM only)")
-# parser.add_argument("--share-vocab", default=1, type=int, help="whether to share the embeddings")
-# parser.add_argument("--scramble-rate", default=0, type=float, help="rate of scrambling")
-# parser.add_argument("--sp-model", help="SP model to load for evaluation")
-# parser.add_argument("--nn-file", help="data file, one example per line")
-
-args = parser.parse_args()
-
 BATCH_SIZE = 128
 entok = MosesTokenizer(lang='en')
+
 
 def print_progress(i):
     if i != 0 and i % BATCH_SIZE == 0:
         sys.stderr.write('.')
         if int(i/BATCH_SIZE) % 50 == 0:
             print(i, file=sys.stderr)
+
 
 def create_embeddings(model, examps):
     """Embed textual examples
@@ -97,36 +59,107 @@ def create_embeddings(model, examps):
     print("", file=sys.stderr)
     return embeddings
 
+
 def calc_similarity_matrix(model, db, quer):
     db_emb = create_embeddings(model, db)
     quer_emb = create_embeddings(model, quer)
     print(f'Performing similarity calculation', file=sys.stderr)
     return np.matmul(quer_emb, np.transpose(db_emb))
 
-with open(args.submission_file, "r") as f:
-    submission_abs = [x.strip() for x in f]
-with open(args.db_file, "r") as f:
-    db = [json.loads(x) for x in f] # for debug
-    db_abs = [x['paperAbstract'] for x in db]
 
-model, epoch = load_model(None, args.model_file)
-model.eval()
-assert not model.training
+def calc_reviewer_db_mapping(reviewers, db, filter_field):
+    print(f'Calculating reviewer-paper mapping for {len(reviewers)} reviewers and {len(db)} papers', file=sys.stderr)
+    mapping = np.zeros( (len(reviewers), len(db)) )
+    reviewer_id_map = defaultdict(lambda: [])
+    for i, reviewer in enumerate(reviewers):
+        reviewer_id_map[reviewer].append(i)
+    for j, entry in enumerate(db):
+        for cols in entry['authors']:
+            if cols[filter_field] in reviewer_id_map:
+                for i in reviewer_id_map[cols[filter_field]]:
+                    mapping[i,j] = 1
+    return mapping
 
-if args.load_matrix:
-    mat = np.load(args.load_matrix)
-else:
-    mat = calc_similarity_matrix(model, db_abs, submission_abs)
-    if args.save_matrix:
-        np.save(args.save_matrix, mat)
 
-for i, query in enumerate(submission_abs):
-    scores = mat[i]
-    best_idxs = scores.argsort()[-5:][::-1]
-    print('-----------------------------------------------------')
-    print('*** Paper Abstract')
-    print(query)
-    print('\n *** Similar Paper Abstracts')
-    for idx in best_idxs:
-        print(f'# Score {scores[idx]}\n{db[idx]}')
-    print()
+def calc_aggregate_reviewer_score(rdb, scores, operator='max'):
+    """Calculate the aggregate reviewer score for one paper
+
+    :param rdb: Reviewer DB. Numpy matrix of reviewers x DB_size
+    :param simscores: Similarity scores between the current paper and the DB. Numpy array of length DB_size
+    :param operator: Which operator to apply (max, weighted_topK)
+    :return: Numpy matrix of length reviewers indicating the score for that reviewer
+    """
+    INVALID_SCORE = 0
+    scored_rdb = rdb * scores.reshape((1, scores.shape[0])) + (1-rdb) * INVALID_SCORE
+    if operator == 'max':
+        agg = np.amax(scored_rdb, axis=1)
+    elif operator.startswith('weighted_top'):
+        k = int(operator[12:])
+        weighting = np.reshape(1/np.array(range(1, k+1)), (1,k))
+        scored_rdb.sort(axis=1)
+        topk = scored_rdb[:,-k:]
+        # print(topk)
+        agg = (topk*weighting).sum(axis=1)
+    else:
+        raise ValueError(f'Unknown operator {operator}')
+    return agg
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--submission_file", type=str, required=True, help="A line-by-line file of submissions")
+    parser.add_argument("--db_file", type=str, required=True, help="File (in s2 json format) of relevant papers from reviewers")
+    parser.add_argument("--reviewer_file", type=str, required=True, help="A file of reviewer files or IDs that can review this time")
+    parser.add_argument("--filter_field", type=str, default="Name", help="Which field to filter on")
+    parser.add_argument("--model_file", help="filename to load the pre-trained semantic similarity file.")
+    parser.add_argument("--save_paper_matrix", help="A filename for where to save the paper similarity matrix")
+    parser.add_argument("--load_paper_matrix", help="A filename for where to load the cached paper similarity matrix")
+    parser.add_argument("--ngrams", default=0, type=int, help="whether to use character n-grams")
+
+    args = parser.parse_args()
+
+    # Load the data
+    with open(args.submission_file, "r") as f:
+        submission_abs = [x.strip() for x in f]
+        submissions = [{'paperAbstract': x} for x in submission_abs]
+    with open(args.reviewer_file, "r") as f:
+        reviewer_names = [x.strip() for x in f]
+    with open(args.db_file, "r") as f:
+        db = [json.loads(x) for x in f]  # for debug
+        db_abs = [x['paperAbstract'] for x in db]
+    rdb = calc_reviewer_db_mapping(reviewer_names, db, 'name')
+
+    # Calculate or load paper similarity matrix
+    if args.load_paper_matrix:
+        mat = np.load(args.load_paper_matrix)
+        assert(mat.shape[0] == len(submission_abs) and mat.shape[1] == len(db_abs))
+    else:
+        print('Loading model', file=sys.stderr)
+        model, epoch = load_model(None, args.model_file)
+        model.eval()
+        assert not model.training
+        mat = calc_similarity_matrix(model, db_abs, submission_abs)
+        if args.save_paper_matrix:
+            np.save(args.save_paper_matrix, mat)
+
+    reviewer_scores = np.zeros( (len(submissions), len(reviewer_names)) )
+    for i, query in enumerate(submissions):
+        scores = mat[i]
+        best_idxs = scores.argsort()[-5:][::-1]
+        print('-----------------------------------------------------')
+        print('*** Paper Abstract')
+        print(query)
+        print('\n*** Similar Paper Abstracts')
+        for idx in best_idxs:
+            print(f'# Score {scores[idx]}\n{db_abs[idx]}')
+        print()
+
+        reviewer_scores[i] = calc_aggregate_reviewer_score(rdb, scores, 'weighted_top3')
+        best_reviewers = reviewer_scores[i].argsort()[-5:][::-1]
+        print('*** Best Matched Reviewers')
+        for idx in best_reviewers:
+            print(f'# {reviewer_names[idx]} (Score {reviewer_scores[i][idx]})')
+        print()
+
