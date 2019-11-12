589262486ae2a221bb3d7b559e31d8f8f7dc6124
Graham Neubig <neubig@gmail.com>
2019-11-12 10:36:37 -0500

    Added capability to filter using regex for speed, but it was slow :(

--- s2_filter.py
+++ s2_filter.py
@@ -1,8 +1,8 @@
 import argparse
 import json
-import numpy as np
 import suggest_utils
 import sys
+import re
 
 if __name__ == "__main__":
 
@@ -10,6 +10,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--reviewer_file", type=str, required=True, help="A tsv file of reviewer names and IDs")
     parser.add_argument("--filter_field", type=str, default="name", help="Which field to filter on (name,id)")
+    parser.add_argument("--regex", action='store_true', help="Use a parsing algorithm with regexes that may not be 100% correct")
 
     args = parser.parse_args()
 
@@ -19,11 +20,19 @@ if __name__ == "__main__":
         reviewer_names = [x[0][0] for x in reviewer_data]
     mapping = suggest_utils.calc_reviewer_id_mapping(reviewer_data, args.filter_field)
 
-    for line in sys.stdin:
-        data = json.loads(line)
-        if args.filter_field == 'id':
-            ok = any([any([(y in mapping) for y in x['ids']]) for x in data['authors']])
-        else:
-            ok = any([(x['name'] in mapping) for x in data['authors']])
-        if ok:
-            print(line.strip())
+    # Fast matching with regex
+    if args.regex:
+        mapping_re = re.compile('"('+'|'.join(mapping.keys())+')"')
+        for line in sys.stdin:
+            if mapping_re.search(line):
+                print(line.strip())
+    # Slow matching with json
+    else:
+        for line in sys.stdin:
+            data = json.loads(line)
+            if args.filter_field == 'id':
+                ok = any([any([(y in mapping) for y in x['ids']]) for x in data['authors']])
+            else:
+                ok = any([(x['name'] in mapping) for x in data['authors']])
+            if ok:
+                print(line.strip())
