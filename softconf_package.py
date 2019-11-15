05c9a091fe96617172782747b61cfa56f9d6b7ec
Graham Neubig <neubig@gmail.com>
2019-11-14 23:16:48 -0500

    Added simple script to export in softconf format

---
+++ softconf_package.py
@@ -0,0 +1,21 @@
+import argparse
+import json
+import csv
+import sys
+import re
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--suggestion_file", type=str, required=True, help="The reviewer CSV file")
+
+    args = parser.parse_args()
+
+    # Load the data
+    with open(args.suggestion_file, "r") as f:
+        submissions = [json.loads(x) for x in f]
+
+    for submission in submissions:
+        sid = submission['startId']
+        print(f'{sid}:'+';'.join([x['startId'] for x in submission['authors']]))
