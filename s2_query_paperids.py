73e1bcd36803b0867a0893b41eb1ffaf4668328f
Arya McCarthy <arya.mccarthy@gmail.com>
2019-11-15 16:01:24 -0500

    Speed up by using persistent requests session

--- s2_query_paperids.py
+++ s2_query_paperids.py
@@ -39,24 +39,26 @@ if __name__ == "__main__":
     rev_len = len(reviewer_ids)
     print(f"Querying s2 for {rev_len} reviewers (.=50 reviewers)", file=sys.stderr)
     sleep_time = 1
-    for i, rid in enumerate(reviewer_ids):
-        r = requests.get(f"http://api.semanticscholar.org/v1/author/{rid}")
-        while r.status_code == 429:
-            sleep_time *= 2
-            print(
-                f"WARNING: Hit rate limit. Increasing sleep to {sleep_time} ms",
-                file=sys.stderr,
-            )
+    with requests.Session() as session:
+        session.verify = False  # Not verifying SSL cert speeds things up.
+        for i, rid in enumerate(reviewer_ids):
+            r = session.get(f"http://api.semanticscholar.org/v1/author/{rid}")
+            while r.status_code == 429:
+                sleep_time *= 2
+                print(
+                    f"WARNING: Hit rate limit. Increasing sleep to {sleep_time} ms",
+                    file=sys.stderr,
+                )
+                time.sleep(sleep_time / 1000.0)
+                r = session.get(f"http://api.semanticscholar.org/v1/author/{rid}")
+            if r.status_code != 200:
+                print(f"WARNING: Could not access rid {rid}", file=sys.stderr)
+            else:
+                user = r.json()
+                # print(json.dumps(user))
+                for paper in user["papers"]:
+                    reviewer_papers.add(paper["paperId"])
+                suggest_utils.print_progress(i, 50)
             time.sleep(sleep_time / 1000.0)
-            r = requests.get(f"http://api.semanticscholar.org/v1/author/{rid}")
-        if r.status_code != 200:
-            print(f"WARNING: Could not access rid {rid}", file=sys.stderr)
-        else:
-            user = r.json()
-            # print(json.dumps(user))
-            for paper in user["papers"]:
-                reviewer_papers.add(paper["paperId"])
-            suggest_utils.print_progress(i, 50)
-        time.sleep(sleep_time / 1000.0)
     for x in reviewer_papers:
         print(x)
