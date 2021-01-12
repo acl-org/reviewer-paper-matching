16b5bb6e5fe861ff7cc1fcdb7a93aa412a4c26fc
C.M. Downey <cmdowney@uw.edu>
2021-01-12 14:52:29 -0800

    Modify paths in training shell script

--- train.sh
+++ train.sh
@@ -2,12 +2,12 @@
 source activate acl-review
 
 bash download_sts17.sh
-python tokenize_abstracts.py --infile res/acl-anthology.json --outfile res/abstracts.txt
-python sentencepiece_abstracts.py --infile res/abstracts.txt --vocab-size 20000 \
-                                  --model-name res/abstracts.sp.20k --outfile res/abstracts.20k.sp.txt 
-python -u train_similarity.py --data-file res/abstracts.20k.sp.txt \
+python reviewer-paper-matching/tokenize_abstracts.py --infile scratch/acl-anthology.json --outfile scratch/abstracts.txt
+python reviewer-paper-matching/sentencepiece_abstracts.py --infile scratch/abstracts.txt --vocab-size 20000 \
+                                  --model-name scratch/abstracts.sp.20k --outfile scratch/abstracts.20k.sp.txt 
+python -u reviewer-paper-matching/train_similarity.py --data-file scratch/abstracts.20k.sp.txt \
                               --model avg --dim 1024 --epochs 20 --ngrams 0 --share-vocab 1 --dropout 0.3 \
-                              --outfile res/similarity-model.pt --batchsize 64 --megabatch-size 1 \
+                              --outfile scratch/similarity-model.pt --batchsize 64 --megabatch-size 1 \
                               --megabatch-anneal 10 --seg-length 1 \
-                              --sp-model res/abstracts.sp.20k.model 2>&1 | tee res/training.log
+                              --sp-model scratch/abstracts.sp.20k.model 2>&1 | tee logs/training.log
 
