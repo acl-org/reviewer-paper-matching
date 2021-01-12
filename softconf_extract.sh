f5e40119a08c186232b6774607ac18c84aae7f9a
C.M. Downey <cmdowney@uw.edu>
2021-01-12 14:52:55 -0800

    Add shell scripts for softconf_extract and suggest_reviewers steps

---
+++ softconf_extract.sh
@@ -0,0 +1,6 @@
+python reviewer-paper-matching/softconf_extract.py \
+    --profile_in=scratch/profiles.csv \
+    --submission_in=scratch/submissions.csv \
+    --reviewer_out=scratch/reviewers.jsonl \
+    --submission_out=scratch/submissions.jsonl \
+    --bid_out=scratch/cois.npy
